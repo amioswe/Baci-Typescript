b04cc91e9ee23732e6597511b69ffb0e619e2a99
amioswe <amioswe@gmail.com>
2026-09-14 23:02:32 +0600

    read-spread typescript

---
+++ read_spread.ts
@@ -0,0 +1,20 @@
+const device:{name:string,price:number,storage:number,color:string}={
+    name: 'iphone',
+    price: 1000,
+    storage: 256,
+    color:'white'
+}
+
+// const  {name:devicename,...deviceinfo}=device 
+// // const detailts=device.name;
+
+// // console.log(detailts);
+// console.log(devicename,deviceinfo);
+
+const numb:number[]=[10,20,30,40];
+
+const num2:string[]=['amio','durjoy','apu','laura'];
+
+const diplay:number[]=[...numb];
+
+console.log(diplay);
