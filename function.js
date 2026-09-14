1ba0302a0288be849db03e52007bcbae52132e62
amioswe <amioswe@gmail.com>
2026-09-14 17:27:26 +0600

    typescript function

---
+++ function.js
@@ -0,0 +1,7 @@
+"use strict";
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+const result = add(5, 10);
+console.log(result);
---
+++ function.ts
@@ -0,0 +1,16 @@
+// function add(num1: number, num2: number): number {
+//     const sum = num1 + num2;
+//     return sum;
+// }
+
+// const result : number = add(5, 10);
+// console.log(result);
+
+
+function calculate(numbers: number[],tax:number,indetails?:string): boolean {
+ return false;
+}
+
+  let addd=calculate([1,2,3,4], 0.1, 'details');
+
+  console.log(addd);
